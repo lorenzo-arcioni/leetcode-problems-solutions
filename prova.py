78ccddc6e9206cefee484c58e284249a3e1249e3
Lorenzo Arcioni <lorenzo.arcioni2000@gmail.com>
2023-10-03 01:23:42 +0200

    ok per oggi

--- prova.py
+++ prova.py
@@ -1,7 +1,7 @@
 import statistics as st
 from bisect import bisect
 
-def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
+def findMedianSortedArrays(nums1: list[int], nums2: list[int]) -> float:
     # Funzione ausiliaria
     def get_median(lst: list[int]) -> float:
 
@@ -22,15 +22,16 @@ def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
         return get_median(nums1)
 
     # La lista nums1 viene prima di nums2
-    if nums1[-1] > nums2[0]:
+    if nums1[-1] > nums2[-1]:
         nums1, nums2 = nums2, nums1
     
     len_nums1 = N = len(nums1)
-    len_nums2 = K = len(nums2) 
+    len_nums2 = K = len(nums2)
+
+    L = len(nums1) + len(nums2) 
     
     # Questo se le due liste sono disgiunte
     if nums1[-1] < nums2[0]:
-        L = len(nums1) + len(nums2)
 
         if L % 2 == 0:
             if L//2 > len_nums1:
@@ -57,20 +58,42 @@ def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
     # Troviamo l'indice in nums1 che corrisponde alla posizione del primo valore della lista nums2
     key_index = bisect(nums1, nums2[0]) - 1 # Se non è presente il valore esatto teniamo l'indice del valore subito prima
 
-    # Ora abbiamo che nums1[key_index:] è la porzione della lista che ha una sovrapposizione con numbs2 
+    # Ora abbiamo che nums1[key_index:] è la porzione della lista che ha una sovrapposizione con nums2.
 
-    
+    # Se key_index > L - key_index, allora la mediana si trova nella prima parte della lista nums1.
 
+    print("Il key_index è: ", key_index)
 
+    if key_index + 1 > L - K - (N - key_index):
 
+        half_index = L//2
 
+        # Quindi possiamo trovare la mediana facendo...
+        if L % 2 == 0:
+            return (nums1[half_index - 1] + nums1[half_index]) / 2
+        else:
+            return nums1[half_index]
+        
+    # Se L - N - (N - key_index) > N + (N - key_index), allora la mediana si trova nella seconda parte della lista nums2.
+    if L - N - (N - key_index) > N + (N - key_index):
+
+        half_index = L//2
+
+        # Quindi possiamo trovare la mediana facendo...
+        if L % 2 == 0:
+            return (nums2[half_index - 1] + nums2[half_index]) / 2
+        else:
+            return nums2[half_index]
+        
+    # Altrimenti vuol dire che la mediana si trova nella sovrapposizione delle due liste.
+    # Sappiamo che la sovrapposizione delle due liste, in sequenza, ha lunghezza pari; in quanto la lunghezza delle due sovrapposizioni è la stessa per nums1 e nums2.
+    # Quindi adesso possiamo trovare la mediana facendo...
 
 
 
             
-nums1 = [9,10000, 900000]
-nums2 = [1, 2, 4, 6]
+nums1 = [1, 2, 3, 4, 5, 6]
+nums2 = [4, 8, 10]
 #print(findMedianSortedArrays(nums1, nums2))
-#print(st.median(sorted(nums1 + nums2)))
-
+print(st.median(sorted(nums1 + nums2)))
 print(findMedianSortedArrays(nums1, nums2))
---
+++ sol-2.py
@@ -0,0 +1,71 @@
+import statistics as st
+from bisect import bisect
+
+def findMedianSortedArrays(nums1: list[int], nums2: list[int]) -> float:
+    
+    L = len(nums1) + len(nums2)
+
+    f = L % 2 == 0
+
+    i = k = r = 0
+
+
+    while i < len(nums1) and k < len(nums2):
+
+        if nums1[i] <= nums2[k]:
+            if r == L // 2 - 1:
+
+                if f:
+                    if i+1 == len(nums1):
+                        return (nums1[i] + nums2[k]) / 2
+                    return (nums1[i] + min(nums1[i+1], nums2[k])) / 2
+                else:
+                    if i+1 == len(nums1):
+                        return nums2[k]
+                    return min(nums1[i+1], nums2[k])            
+            i += 1
+            
+        elif nums1[i] > nums2[k]:
+                
+            if r == L // 2 - 1:
+                if f:
+                    if k +1 == len(nums2):
+                        return (nums2[k] + nums1[i]) / 2
+                    return (nums2[k] + min(nums1[i], nums2[k+1])) / 2
+                else:
+                    if k+1 == len(nums2):
+                        return nums1[i]
+                    return min(nums1[i], nums2[k+1])
+            
+            k += 1
+
+        r += 1
+
+        if i >= len(nums1):
+            while k < len(nums2):
+                if r == L // 2 - 1:
+                    if f:
+                        return (nums2[k] + nums2[k+1]) / 2
+                    else:
+                        return nums2[k+1]
+                r += 1
+                k += 1
+
+        if k >= len(nums2):
+            while i < len(nums1):
+                if r == L // 2 - 1:
+                    if f:
+                        return (nums1[i] + nums1[i+1]) / 2
+                    else:
+                        return nums1[i+1]
+                r += 1
+                i += 1
+
+            
+
+
+nums1 = [1,2]
+nums2 = [3,4]
+#print(findMedianSortedArrays(nums1, nums2))
+print(st.median(sorted(nums1 + nums2)))
+print(findMedianSortedArrays(nums1, nums2))
